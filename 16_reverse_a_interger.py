5091435ae61891d4dece0846eedcfdc0e5065966
Kosalvireak Khoeun <virakvary@gmail.com>
2022-07-15 15:18:53 +0700

    add exercises 16&17&18

--- 16_reverse_a_interger.py
+++ 16_reverse_a_interger.py
@@ -1,11 +1,12 @@
 num = 5642
 new_num = 0
 while num != 0:
-    remainder = num % 10
-    # new_num = new_num  + remainder *i
+    # remainder = num % 10
     # or
+    #i = 1
     # new_num = new_num + (num % 10)*i
+    #i = i *10
     # or
-    new_num = new_num * 10 + remainder
+    new_num = new_num * 10 + num % 10
     num = num // 10
 print(new_num)
